43153239f7a6d2802ac70a4b9f06a8e20bf32016
Rinnegatamante <rinnegatamante@gmail.com>
2020-04-20 22:19:51 +0200

    Caching rom selector menu entries.

--- Source/SysVita/UI/MainMenuScreen.cpp
+++ Source/SysVita/UI/MainMenuScreen.cpp
@@ -33,6 +33,13 @@
 
 char selectedRom[512];
 
+struct RomSelection {
+	char name[128];
+	RomSelection *next;
+};
+
+RomSelection *list = nullptr;
+
 char *DrawRomSelector() {
 	bool selected = false;
 	
@@ -43,28 +50,40 @@ char *DrawRomSelector() {
 	ImGui::SetNextWindowSize(ImVec2(SCR_WIDTH, SCR_HEIGHT - 19), ImGuiSetCond_Always);
 	ImGui::Begin("", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoBringToFrontOnFocus);
 	
-	std::string			full_path;
+	if (!list) {
+		std::string			full_path;
 
-	IO::FindHandleT		find_handle;
-	IO::FindDataT		find_data;
+		IO::FindHandleT		find_handle;
+		IO::FindDataT		find_data;
 	
-	if(IO::FindFileOpen( DAEDALUS_VITA_PATH("Roms/"), &find_handle, find_data ))
-	{
-		do
+		if(IO::FindFileOpen( DAEDALUS_VITA_PATH("Roms/"), &find_handle, find_data ))
 		{
-			const char * rom_filename( find_data.Name );
-			if(IsRomfilename( rom_filename ))
+			do
 			{
-				if (ImGui::Button(rom_filename)){
-					sprintf(selectedRom, rom_filename);
-					selected = true;
+				const char * rom_filename( find_data.Name );
+				if(IsRomfilename( rom_filename ))
+				{
+					RomSelection *node = (RomSelection*)malloc(sizeof(RomSelection));
+					sprintf(node->name, rom_filename);
+					node->next = list;
+					list = node;
 				}
 			}
-		}
-		while(IO::FindFileNext( find_handle, find_data ));
+			while(IO::FindFileNext( find_handle, find_data ));
 
-		IO::FindFileClose( find_handle );
+			IO::FindFileClose( find_handle );
+		}
 	}
+	
+	RomSelection *p = list;
+	while (p) {
+		if (ImGui::Button(p->name)){
+			sprintf(selectedRom, p->name);
+			selected = true;
+		}
+		p = p->next;
+	}
+
 	ImGui::End();
 		
 	glViewport(0, 0, static_cast<int>(ImGui::GetIO().DisplaySize.x), static_cast<int>(ImGui::GetIO().DisplaySize.y));
@@ -72,6 +91,15 @@ char *DrawRomSelector() {
 	ImGui_ImplVitaGL_RenderDrawData(ImGui::GetDrawData());
 	vglStopRendering();
 	
-	if (selected) return selectedRom;
+	if (selected) {
+		p = list;
+		while (p) {
+			RomSelection *old = p;
+			p = p->next;
+			free(old);
+		}
+		list = nullptr;
+		return selectedRom;
+	}
 	return nullptr;
 }
