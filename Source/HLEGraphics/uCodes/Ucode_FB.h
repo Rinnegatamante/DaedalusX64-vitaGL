c0546a1efa1419d73b3bb75abacb3d120eca202e
Rinnegatamante <rinnegatamante@gmail.com>
2020-05-15 12:24:32 +0200

    Make cpu rendering properly render subsequent frames.

--- Source/HLEGraphics/uCodes/Ucode_FB.h
+++ Source/HLEGraphics/uCodes/Ucode_FB.h
@@ -26,13 +26,13 @@ static inline CRefPtr<CNativeTexture> LoadFrameBuffer(u32 origin)
 	u32 width  = Memory_VI_GetRegister( VI_WIDTH_REG );
 	if( width == 0 )
 	{
-		//DAEDALUS_ERROR("Loading 0 size frame buffer?");
+		//DBGConsole_Msg(0, "Loading 0 size frame buffer?");
 		return NULL;
 	}
 
 	if( origin <= width*2 )
 	{
-		//DAEDALUS_ERROR("Loading small frame buffer not supported");
+		//DBGConsole_Msg(0, "Loading small frame buffer not supported");
 		return NULL;
 	}
 	//ToDO: We should use uViWidth+1 and uViHeight+1
@@ -64,13 +64,36 @@ static inline CRefPtr<CNativeTexture> LoadFrameBuffer(u32 origin)
 void RenderFrameBuffer(u32 origin)
 {
 	gRenderer->BeginScene();
-
-	CRefPtr<CNativeTexture> texture = LoadFrameBuffer(origin);
-	if(texture != NULL)
-		gRenderer->Draw2DTexture(0, 0, FB_WIDTH, FB_HEIGHT, 0, 0, FB_WIDTH, FB_HEIGHT, texture);
 	
+	CRefPtr<CNativeTexture> texture = LoadFrameBuffer(origin);
+	if(texture != NULL) {
+		
+		u32 corrected_width = GetNextPowerOf2(FB_WIDTH);
+		u32 corrected_height = GetNextPowerOf2(FB_HEIGHT);
+		
+		// LoadFrameBuffer caching makes the framebuffer not properly update, so we do it manually
+		u16 * pixels = (u16*)malloc(corrected_width * corrected_height * sizeof(u16));
+		u32 src_offset = 0;
+
+		for (u32 y = 0; y < FB_HEIGHT; ++y)
+		{
+			u32 dst_row_offset = y * corrected_width;
+			u32 dst_offset     = dst_row_offset;
+
+			for (u32 x = 0; x < Memory_VI_GetRegister( VI_WIDTH_REG ); ++x)
+			{
+				pixels[dst_offset] = (g_pu8RamBase[(origin + src_offset)^U8_TWIDDLE]<<8) | g_pu8RamBase[(origin + src_offset+  1)^U8_TWIDDLE] | 1;  // NB: or 1 to ensure we have alpha
+				dst_offset += 1;
+				src_offset += 2;
+			}
+		}
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, corrected_width, corrected_height, 0, GL_RGBA, GL_UNSIGNED_SHORT_5_5_5_1, pixels);
+
+		gRenderer->Draw2DTexture(FB_WIDTH - Memory_VI_GetRegister( VI_WIDTH_REG ), 0, FB_WIDTH, FB_HEIGHT, 0, 0, FB_WIDTH, FB_HEIGHT, texture);
+		free(pixels);
+	}
 	gRenderer->EndScene();
-	gGraphicsPlugin->UpdateScreen();
+	CGraphicsContext::Get()->UpdateFrame( false );
 }
 
 #endif // !DAEDALUS_PSP
