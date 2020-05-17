2481af44f003b480880b1291ddecafd9c6555de2
Rinnegatamante <rinnegatamante@gmail.com>
2020-05-17 21:57:14 +0200

    Reworked CPU rendering code.

--- Source/HLEGraphics/uCodes/Ucode_FB.h
+++ Source/HLEGraphics/uCodes/Ucode_FB.h
@@ -20,45 +20,46 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef HLEGRAPHICS_UCODES_UCODE_FB_H_
 #define HLEGRAPHICS_UCODES_UCODE_FB_H_
 
+#define FB_WIDTH  320
+#define FB_HEIGHT 240
+
+static float fb_ratio;
+
 #if !defined(DAEDALUS_PSP)
 static inline CRefPtr<CNativeTexture> LoadFrameBuffer(u32 origin)
 {
 	u32 width  = Memory_VI_GetRegister( VI_WIDTH_REG );
-	if( width == 0 )
-	{
-		//DBGConsole_Msg(0, "Loading 0 size frame buffer?");
+	
+	if (width == 0 || (origin <= width*2))
 		return NULL;
-	}
+	
+	fb_ratio = (float)FB_WIDTH / (float)width;
+	u32 height = (u32)((float)FB_HEIGHT * fb_ratio);
+	
+	CRefPtr<CNativeTexture> texture = CNativeTexture::Create(width, height, TexFmt_8888 );
+	u32 tex_width = texture->GetCorrectedWidth();
+	u32 tex_height = texture->GetCorrectedHeight();
+	texture->InstallTexture();
+	
+	u16 *pixels = (u16*)malloc(tex_width * tex_height * sizeof(u16));
+	u32 src_offset = 0;
 
-	if( origin <= width*2 )
+	for (u32 y = 0; y < height; ++y)
 	{
-		//DBGConsole_Msg(0, "Loading small frame buffer not supported");
-		return NULL;
+		u32 dst_row_offset = y * tex_width;
+		u32 dst_offset     = dst_row_offset;
+
+		for (u32 x = 0; x < width; ++x)
+		{
+			pixels[dst_offset] = (g_pu8RamBase[(origin + src_offset)^U8_TWIDDLE]<<8) | g_pu8RamBase[(origin + src_offset+  1)^U8_TWIDDLE] | 1;  // NB: or 1 to ensure we have alpha
+			dst_offset += 1;
+			src_offset += 2;
+		}
 	}
-	//ToDO: We should use uViWidth+1 and uViHeight+1
-#define FB_WIDTH  320
-#define FB_HEIGHT 240
-
-#ifdef DAEDALUS_ENABLE_ASSERTS
-	DAEDALUS_ASSERT(g_CI.Size == G_IM_SIZ_16b,"32b frame buffer is not supported");
-	//DAEDALUS_ASSERT((uViWidth+1) == FB_WIDTH,"Variable width is not handled");
-	//DAEDALUS_ASSERT((uViHeight+1) == FB_HEIGHT,"Variable height is not handled");
-#endif
-	TextureInfo ti;
-
-	ti.SetSwapped			(0);
-	ti.SetPalette			(0);
-	ti.SetTlutAddress		(gTlutLoadAddresses[0]);
-	ti.SetTLutFormat		(kTT_RGBA16);
-	ti.SetFormat			(0);
-	ti.SetSize				(2);
-
-	ti.SetLoadAddress		(origin - width * 2);
-	ti.SetWidth				(FB_WIDTH);
-	ti.SetHeight			(FB_HEIGHT);
-	ti.SetPitch				((width << 2) >> 1);
-
-	return gRenderer->LoadTextureDirectly(ti);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex_width, tex_height, 0, GL_RGBA, GL_UNSIGNED_SHORT_5_5_5_1, pixels);
+	free(pixels);
+	
+	return texture;
 }
 
 void RenderFrameBuffer(u32 origin)
@@ -66,32 +67,9 @@ void RenderFrameBuffer(u32 origin)
 	gRenderer->BeginScene();
 	
 	CRefPtr<CNativeTexture> texture = LoadFrameBuffer(origin);
-	if(texture != NULL) {
-		
-		u32 corrected_width = GetNextPowerOf2(FB_WIDTH);
-		u32 corrected_height = GetNextPowerOf2(FB_HEIGHT);
-		
-		// LoadFrameBuffer caching makes the framebuffer not properly update, so we do it manually
-		u16 * pixels = (u16*)malloc(corrected_width * corrected_height * sizeof(u16));
-		u32 src_offset = 0;
+	if(texture != NULL)
+		gRenderer->Draw2DTexture(FB_WIDTH - Memory_VI_GetRegister( VI_WIDTH_REG ), FB_HEIGHT - ((float)FB_HEIGHT * fb_ratio), FB_WIDTH + 1, FB_HEIGHT + 1, 0, 0, Memory_VI_GetRegister( VI_WIDTH_REG ), FB_HEIGHT, texture);
 
-		for (u32 y = 0; y < FB_HEIGHT; ++y)
-		{
-			u32 dst_row_offset = y * corrected_width;
-			u32 dst_offset     = dst_row_offset;
-
-			for (u32 x = 0; x < Memory_VI_GetRegister( VI_WIDTH_REG ); ++x)
-			{
-				pixels[dst_offset] = (g_pu8RamBase[(origin + src_offset)^U8_TWIDDLE]<<8) | g_pu8RamBase[(origin + src_offset+  1)^U8_TWIDDLE] | 1;  // NB: or 1 to ensure we have alpha
-				dst_offset += 1;
-				src_offset += 2;
-			}
-		}
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, corrected_width, corrected_height, 0, GL_RGBA, GL_UNSIGNED_SHORT_5_5_5_1, pixels);
-
-		gRenderer->Draw2DTexture(FB_WIDTH - Memory_VI_GetRegister( VI_WIDTH_REG ), 0, FB_WIDTH, FB_HEIGHT, 0, 0, FB_WIDTH, FB_HEIGHT, texture);
-		free(pixels);
-	}
 	gRenderer->EndScene();
 	CGraphicsContext::Get()->UpdateFrame( false );
 }
