95824c46690aa59742b34edf6e114a7cbde935f9
Rinnegatamante <rinnegatamante@gmail.com>
2020-06-08 15:29:01 +0200

    Fix for malformed args.txt generation on some consoles.

--- Launcher/builder/build.bat
+++ Launcher/builder/build.bat
@@ -1,6 +1,6 @@
 @set /p title="Insert bubble name: "
 @set /p rom="Insert rom name: "
 @set /p id="Insert bubble title ID (9 characters [NOTE: Only UPPERCASE letters or numbers]): "
-@echo %rom% > "assets/args.txt"
+@echo|set /p="%rom%"> "assets/args.txt"
 vita-mksfoex -s TITLE_ID=%id% "%title%" param.sfo
 vita-pack-vpk -s param.sfo -b eboot.bin "%title%.vpk" -a assets/icon0.png=sce_sys/icon0.png -a assets/bg.png=sce_sys/livearea/contents/bg.png -a assets/startup.png=sce_sys/livearea/contents/startup.png -a assets/template.xml=sce_sys/livearea/contents/template.xml -a assets/args.txt=args.txt
