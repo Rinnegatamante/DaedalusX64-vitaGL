b9bc74a1ee5318da59115685c816562725a6704f
Rinnegatamante <rinnegatamante@gmail.com>
2020-06-23 16:51:33 +0200

    Improved memcpy_byteswap performances.

--- Source/Utility/FastMemcpy.cpp
+++ Source/Utility/FastMemcpy.cpp
@@ -8,12 +8,14 @@ homepage: http://wordpress.fx-world.org
 */
 
 #include "stdafx.h"
-#include "FastMemcpy.h"
+
 
 #include "Utility/DaedalusTypes.h"
 #include "Utility/Endian.h"
+#include "Utility/FastMemcpy.h"
 #include "Utility/Timing.h"
 
+#include <cstring>
 #include <stdio.h>
 
 //*****************************************************************************
@@ -22,11 +24,9 @@ homepage: http://wordpress.fx-world.org
 //*****************************************************************************
 void memcpy_byteswap( void* dst, const void* src, size_t size )
 {
-
 	u8* src8 = (u8*)src;
 	u8* dst8 = (u8*)dst;
-	u32* src32;
-	u32* dst32;
+
 	// < 4 isn't worth trying any optimisations...
 	if(size>=4)
 	{
@@ -36,102 +36,109 @@ void memcpy_byteswap( void* dst, const void* src, size_t size )
 			*(u8*)((uintptr_t)dst8++ ^ U8_TWIDDLE) = *(u8*)((uintptr_t)src8++ ^ U8_TWIDDLE);
 			size--;
 		}
+		
 		// We are dst aligned now but need at least 4 bytes to copy
 		if(size>=4)
 		{
-			src32 = (u32*)src8;
-			dst32 = (u32*)dst8;
-			u32 srcTmp;
-			u32 dstTmp;
-			u32 size32 = size >> 2;
-			size &= 0x3;
-			switch( (uintptr_t)src8&0x3 )
+			u32 src_alignment = (uintptr_t)src8&0x3;
+			if (src_alignment == 0)		// We are now both dst and src aligned and >= 4 bytes to copy 
 			{
-				case 0:	//Both src and dst are aligned to 4 bytes
-					{
-#if 0 //DAEDALUS_W32
-						// MSVC11 memcpy is almost 50% faster! It takes advantage of SSE2
-						// Mmm breaks wipeout
-						memcpy(dst, src, size);
+#if defined(DAEDALUS_POSIX) || defined(DAEDALUS_W32) || defined(DAEDALUS_VITA)
+				u32 size_aligned = (size & ~0x3);
+				
+				// memcpy is almost 50% faster for windows and linux
+				memcpy_neon(dst8, src8, size_aligned);
+				src8 += size_aligned;
+				dst8 += size_aligned;
 #else
-						//This is faster than PSP's GCC memcpy
-						//ToDo: Profile for other plaforms to see if memcpy is faster
-						while (size32&0x3)
-						{
-							*dst32++ = *src32++;
-							size32--;
-						}
+				//This is faster than the PSP's GCC memcpy
+				//TODO: Profile for other plaforms to see if memcpy is faster
+				u32* src32 = (u32*)src8;
+				u32* dst32 = (u32*)dst8;
 
-						u32 size128 = size32 >> 2;
-						while (size128--)
-						{
-							*dst32++ = *src32++;
-							*dst32++ = *src32++;
-							*dst32++ = *src32++;
-							*dst32++ = *src32++;
-						}
+				u32 size32 = size >> 2;
+				while (size32 & 0x3)
+				{
+					*dst32++ = *src32++;
+					size32--;
+				}
 
-						src8 = (u8*)src32;
+				u32 size128 = size32 >> 2;
+				while (size128--)
+				{
+					dst32[0] = src32[0];
+					dst32[1] = src32[1];
+					dst32[2] = src32[2];
+					dst32[3] = src32[3];
+					src32 += 4;
+					dst32 += 4;
+				}
+				src8 = (u8*)src32;
+				dst8 = (u8*)dst32;
 #endif
-					}
-					break;
-
-				case 1:	//Handle offset by 1
-					{
-						src32 = (u32*)((u32)src8 & ~0x3);
-						srcTmp = *src32++;
-						while(size32--)
-						{
-							dstTmp = srcTmp << 8;
-							srcTmp = *src32++;
-							dstTmp |= srcTmp >> 24;
-							*dst32++ = dstTmp;
-						}
-						src8 = (u8*)src32 - 3;
-					}
-					break;
-
-				case 2:	//Handle offset by 2
-					{
-						src32 = (u32*)((u32)src8 & ~0x3);
-						srcTmp = *src32++;
-						while(size32--)
-						{
-							dstTmp = srcTmp << 16;
-							srcTmp = *src32++;
-							dstTmp |= srcTmp >> 16;
-							*dst32++ = dstTmp;
-						}
-						src8 = (u8*)src32 - 2;
-					}
-					break;
-
-				case 3:	//Handle offset by 3
-					{
-						src32 = (u32*)((u32)src8 & ~0x3);
-						srcTmp = *src32++;
-						while(size32--)
-						{
-							dstTmp = srcTmp << 24;
-							srcTmp = *src32++;
-							dstTmp |= srcTmp >> 8;
-							*dst32++ = dstTmp;
-						}
-						src8 = (u8*)src32 - 1;
-					}
-					break;
 			}
-			dst8 = (u8*)dst32;
+			else	// We are now dst aligned and src unligned and >= 4 bytes to copy
+			{
+				u32* src32 = (u32*)((uintptr_t)src8 & ~0x3);
+				u32* dst32 = (u32*)dst8;
+				u32 srcTmp = *src32++;
+				u32 dstTmp = 0;
+				u32 size32 = size >> 2;
+	
+				switch( src_alignment )
+				{
+				case 1:
+					while(size32--)
+					{
+						dstTmp = srcTmp << 8;
+						srcTmp = *src32++;
+						*dst32++ = dstTmp | (srcTmp >> 24);
+					}
+					break;
+				case 2:
+					while(size32--)
+					{
+						dstTmp = srcTmp << 16;
+						srcTmp = *src32++;
+						*dst32++ = dstTmp | (srcTmp >> 16);
+					}
+					break;
+				case 3:
+					while(size32--)
+					{
+						dstTmp = srcTmp << 24;
+						srcTmp = *src32++;
+						*dst32++ = dstTmp | (srcTmp >> 8);
+						
+					}
+					break;
+				}
+				src8 = (u8*)src32 - (4-src_alignment);
+				dst8 = (u8*)dst32;
+			}
 		}
 	}
 
-	// Copy the remaing byte by byte...
+	// Copy any remaining byte by byte...
+	size &= 0x03;
 	while(size--)
 	{
 		*(u8*)((uintptr_t)dst8++ ^ U8_TWIDDLE) = *(u8*)((uintptr_t)src8++ ^ U8_TWIDDLE);
 	}
 }
+
 #ifdef PROFILE_MEMCPY
+void byteswap_copy( void* dst, const void* src, size_t size )
+{
+	u8* src8 = (u8*)src;
+	u8* dst8 = (u8*)dst;
+
+	while(size--)
+	{
+		*(u8*)((uintptr_t)dst8++ ^ U8_TWIDDLE) = *(u8*)((uintptr_t)src8++ ^ U8_TWIDDLE);
+	}
+}
+
 static inline u64 GetCurrent()
 {
     u64 tick;
@@ -144,18 +151,18 @@ static inline u64 GetCurrent()
 	{																			\
 		u64 time;																\
 		NTiming::GetPreciseTime(&time);											\
-		for (u32 j=0; j<100; ++j)												\
-			fast_memcpy_swizzle(d, s, n);										\
+		for (u32 j=0; j<10000; ++j)												\
+			memcpy_byteswap(d, s, n);											\
 		_fast_memcpy_swizzle = (u32)(GetCurrent()-time);						\
 	}																			\
-	u32 _memcpy_byteswap = 0;													\
+	u32 _copy_byteswap = 0;														\
 	{																			\
 		u64 time = GetCurrent();												\
-		for (u32 j=0; j<100; ++j)												\
-			memcpy_byteswap(d, s, n);											\
-		_memcpy_byteswap = (u32)(GetCurrent()-time);							\
+		for (u32 j=0; j<10000; ++j)												\
+			byteswap_copy(d, s, n);												\
+		_copy_byteswap = (u32)(GetCurrent()-time);								\
 	}																			\
-	printf("%d bytes | SWIZZLE %d | FASTSWIZZLE %d\n", n, _memcpy_byteswap, _fast_memcpy_swizzle); \
+	printf("%ld bytes | BYTESWAP COPY %d | MEMCPY SWIZZLE %d\n", n, _copy_byteswap, _fast_memcpy_swizzle); \
 	}
 
 void memcpy_test( void * dst, const void * src, size_t size )
